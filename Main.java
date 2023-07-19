c82db01acef2c447c3d12edbddeea8d1de18a335
BackDad <80159634+BackDad@users.noreply.github.com>
2023-07-19 16:59:08 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,52 +3,77 @@ package org.example;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Scanner;
+import java.util.regex.Pattern;
 
-class InvalidDataFormatException extends Exception {
-    public InvalidDataFormatException(String message) {
-        super(message);
-    }
-}
-
-public class Main {
+public class UserInfoApp {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
         try {
-            System.out.println("Введите данные в формате: Фамилия Имя Отчество датарождения номертелефона пол");
+            Scanner scanner = new Scanner(System.in);
+
+            System.out.print("Введите данные: ");
             String input = scanner.nextLine();
 
             String[] data = input.split(" ");
+
             if (data.length != 6) {
-                throw new InvalidDataFormatException("Неверное количество данных. Ожидалось 6 значений.");
+                System.out.println("Ошибка: недостаточно данных или лишние данные");
+                return;
             }
 
             String lastName = data[0];
             String firstName = data[1];
             String middleName = data[2];
-            String birthDate = data[3];
-            long phoneNumber = Long.parseLong(data[4]);
-            char gender = data[5].charAt(0);
+            String dateOfBirth = data[3];
+            String phoneNumber = data[4];
+            String gender = data[5];
 
-            if (gender != 'f' && gender != 'm') {
-                throw new InvalidDataFormatException("Неверный формат пола. Поле 'пол' должно быть 'f' или 'm'.");
+            if (!isValidDateFormat(dateOfBirth)) {
+                throw new InvalidDataFormatException("Ошибка: некорректный формат даты рождения");
             }
 
-            String fileName = lastName + ".txt";
-            String content = lastName + " " + firstName + " " + middleName + " " + birthDate + " " + phoneNumber + " " + gender;
-
-            try (FileWriter writer = new FileWriter(fileName, true)) {
-                writer.write(content + System.lineSeparator());
-                System.out.println("Данные успешно записаны в файл " + fileName);
-            } catch (IOException e) {
-                System.out.println("Ошибка при записи в файл: " + e.getMessage());
+            if (!isValidPhoneNumberFormat(phoneNumber)) {
+                throw new InvalidDataFormatException("Ошибка: некорректный формат номера телефона");
             }
+
+            if (!isValidGender(gender)) {
+                throw new InvalidDataFormatException("Ошибка: некорректное значение пола");
+            }
+
+            String filename = lastName + ".txt";
+            FileWriter fileWriter = new FileWriter(filename, true);
+
+            String userInfo = lastName + " " + firstName + " " + middleName + " " +
+                    dateOfBirth + " " + phoneNumber + " " + gender;
+
+            fileWriter.write(userInfo + "\n");
+
+            fileWriter.close();
+
+            System.out.println("Данные сохранены в файл: " + filename);
+        } catch (IOException e) {
+            System.out.println("Ошибка при чтении/записи файла: " + e.getMessage());
         } catch (InvalidDataFormatException e) {
-            System.out.println("Ошибка в формате данных: " + e.getMessage());
-        } catch (NumberFormatException e) {
-            System.out.println("Неверный формат номера телефона. Поле 'номертелефона' должно быть целым числом.");
-        } catch (Exception e) {
-            System.out.println("Ошибка: " + e.getMessage());
+            System.out.println(e.getMessage());
         }
+    }
+
+    private static boolean isValidDateFormat(String date) {
+        String regex = "^(0[1-9]|[12][0-9]|3[01])\\.(0[1-9]|1[0-2])\\.\\d{4}$";
+        return Pattern.matches(regex, date);
+    }
+
+    private static boolean isValidPhoneNumberFormat(String phoneNumber) {
+        String regex = "^[0-9]+$";
+        return Pattern.matches(regex, phoneNumber);
+    }
+
+    private static boolean isValidGender(String gender) {
+        return gender.equalsIgnoreCase("f") || gender.equalsIgnoreCase("m");
+    }
+}
+
+class InvalidDataFormatException extends Exception {
+    public InvalidDataFormatException(String message) {
+        super(message);
     }
 }
